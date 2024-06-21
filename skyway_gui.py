7be7ac6c9f54abaa8717fae20e79fdeda91909d6
Trung Nguyen <ndtrung@uchicago.edu>
2024-06-21 00:13:01 -0500

    Updated skyway dashboard with the slurm backend

--- skyway_gui.py
+++ skyway_gui.py
@@ -33,23 +33,23 @@ class InstanceDescriptor:
         self.vendor_name = vendor_name
 
         self.account = None
-        if 'aws' in account_name:
+        if 'aws' in vendor_name:
             self.account = AWS(account_name)
-        elif 'gcp' in account_name:
+        elif 'gcp' in vendor_name:
             self.account = GCP(account_name)
-        elif 'azure' in account_name:
+        elif 'azure' in vendor_name:
             self.account = AZURE(account_name)
+        elif 'midway3' in vendor_name:
+            self.account = SLURMCluster(account_name)
 
         self.user = os.environ['USER']
 
     def submitJob(self):
         #st.warning("Do you want to create this instance?")
         #if st.button("Yes"):
-        if "midway3" in self.vendor_name:
-            self.connectJob([self.jobname])
-        else:
-            print(f"creating node from {self.vendor_name} with account {self.account_name}")
-            self.account.create_nodes(self.node_type, [self.jobname], need_confirmation=False, walltime=self.walltime)
+        
+        print(f"creating node from {self.vendor_name} with account {self.account_name}")
+        self.account.create_nodes(self.node_type, [self.jobname], need_confirmation=False, walltime=self.walltime)
         initializing = True
         return initializing
 
@@ -57,29 +57,10 @@ class InstanceDescriptor:
         '''
         only get on a on-premise compute node for now with rcc-staff
         '''
-        ppn = 1
-        mem = 4
-        partition = "caslake"
+        
         if "midway3" in self.vendor_name:
-            if "t1" in self.node_type:
-                ppn = 1
-                mem = 4
-            elif "c4" in self.node_type:
-                ppn = 4
-                mem = 16
-            elif "c16" in self.node_type:
-                ppn = 16
-                mem = 64
-            elif "c48" in self.node_type:
-                ppn = 48
-                mem = 128
-            elif "bigmeme" in self.node_type:
-                ppn = 16
-                mem = 512
-                partition = "bigmem"
-            cmd = "gnome-terminal --title='Connecting to the node' -- bash -c "
-            cmd += f" 'sinteractive -J {self.jobname} -A rcc-staff -p {partition} --nodes=1 --ntasks-per-node={ppn} --mem={mem}GB -t {self.walltime}' "
-            os.system(cmd)
+            instanceID = self.account.get_host_ip(self.jobname)
+            self.account.connect_node(instanceID)
 
         elif "aws" in self.vendor_name:
             instanceID = self.account.get_instance_ID(self.jobname)
@@ -91,19 +72,15 @@ class InstanceDescriptor:
 
     def terminateJob(self, node_names = []):
         st.write(f"Terminating instances {node_names}...")
-        self.account.destroy_nodes(node_names=node_names, need_confirmation=False)
+        if "midway3" in self.vendor_name:
+            instanceID = self.account.get_instance_ID(self.jobname)
+            self.account.destroy_nodes(IDs=[instanceID], need_confirmation=False)
+        else:
+            self.account.destroy_nodes(node_names=node_names, need_confirmation=False)
 
     def getBalance(self):
         # retrieve from database for the given account
-        if "midway3" in self.vendor_name:
-
-            cmd = "rcchelp balance -a rcc-staff | awk \'$1 == \"rcc-staff\" {print $4}\' "
-            proc = subprocess.Popen([cmd], stdout=subprocess.PIPE, shell=True)
-            (out, err) = proc.communicate()
-            remaining_balance = float(out)
-        else:
-            #budget = self.account.get_budget(user_name=self.user, verbose=False)
-            accumulating_cost, remaining_balance = self.account.get_cost_and_usage_from_db(user_name=self.user)
+        accumulating_cost, remaining_balance = self.account.get_cost_and_usage_from_db(user_name=self.user)
         return remaining_balance
 
     def getEstimateCost(self):
@@ -118,23 +95,7 @@ class InstanceDescriptor:
   
     def list_nodes(self):
         if "midway3" in self.vendor_name:
-            nodes = []
-            cmd = f"squeue -u {self.user}"
-            proc = subprocess.Popen([cmd], stdout=subprocess.PIPE, shell=True)
-            (out, err) = proc.communicate()
-            
-            cmd = f"export SQUEUE_FORMAT=\"%13i %.4t %24j %13i %N 0.0 0.0\"; squeue -u {self.user} | tail -n1"
-            proc = subprocess.Popen([cmd], stdout=subprocess.PIPE, shell=True)
-            (out, err) = proc.communicate()
-            # encode output as utf-8 from bytes, remove newline character
-            m = out.decode('utf-8').strip()
-            # convert to a list
-            m = m.split()
-            if len(m) == 7:
-                m[0] = self.jobname
-                m[2] = self.node_type
-                nodes = [m]
-
+            nodes, list_of_nodes = self.account.list_nodes(verbose=False) 
         else:
             nodes, list_of_nodes = self.account.list_nodes(verbose=False) 
         return nodes
@@ -182,7 +143,7 @@ if __name__ == "__main__":
             accounts = ('rcc-azure', 'ndtrung-azure')
         elif 'midway3' in vendor_name:
             node_types = ('t1 (1 CPU core + 4 GB RAM)', 'c4 (4 CPU cores + 16 GB RAM)', 'c16 (16 CPU cores + 64 GB RAM)', 'c48 (48 CPU cores + 128 GB RAM)', 'g1 (8 CPU cores + 1 V100 GPU)', 'bigmem (16 CPU cores + 512 GB RAM)')
-            accounts = ('rcc-staff',)
+            accounts = ('rcc-midway3',)
             vendor_short = "midway3"
 
         # account or allocation
